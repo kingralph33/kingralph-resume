9cd86245645c307021b4762f693b423e6390df18
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2024-04-22 22:53:27 -0400

    add resume-data skeleton

---
+++ src/data/resume-data.tsx
@@ -0,0 +1,37 @@
+export const RESUME_DATA = {
+  name: "Ralph King Jr",
+  summary: "",
+  personalWebsite: "https://kingralph.dev",
+  contactMe: {
+    phone: "",
+    email: "ralph@kingralph.dev",
+    location: "Washington DC Metro Area",
+  },
+  work: [
+    {
+      company: "",
+      location: "",
+      companyUrl: "",
+      badge: [],
+      jobTitle: "",
+      startDate: "",
+      endDate: "",
+      description: "",
+    },
+  ],
+  skills: ["JavaScript", "TypeScript", "Microsoft Azure", "NodeJS", "Python"],
+  certifications: [
+    {
+      azurecert900:
+        "https://learn.microsoft.com/api/credentials/share/en-us/KingJrRalphFSIS-8974/B9FC9F5037636B30?sharingId=12CE0D4A3AFD6695",
+    },
+  ],
+  projects: [
+    {
+      title: "",
+      description: "",
+      url: "",
+      techStack: ["", ""],
+    },
+  ],
+} as const;
